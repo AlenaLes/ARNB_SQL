62b5be27cc690ba9568ffbf9c092e4a6d92408b1
Alena Lesikhina <alena.lesikhina@gmail.com>
2024-03-04 00:22:24 +0300

    litttle update

--- Task_distance.sql
+++ Task_distance.sql
@@ -1,21 +1,19 @@
--- Task_distance
--- Цель: Найти топ-10 квартир, максимально приближанных к центру города (ориентир - 13.4050, 52.5200).
--- Привести данные к нужному формату.
--- Вывести ценник за неделю по каждому пункту (учитываем минимально возможное кол-во дней для брони - 6).
-
+/*
+Task_distance
+Цель: Найти топ-10 квартир, максимально приближанных к центру города (ориентир - 13.4050, 52.5200).
+Привести данные к нужному формату.
+Вывести ценник за неделю по каждому пункту (учитываем минимально возможное кол-во дней для брони - 6).
+*/
 SELECT 
     id,
     host_id,
     room_type,
-    cast(minimum_nights as integer) as  minimum_nights,
-    (toFloat64OrNull(replaceRegexpAll(price, '[$,]', '')))*7 as week_price,
-    toFloat64OrNull(latitude) as latitude,
-    toFloat64OrNull(longitude) as longitude,
-    geoDistance(13.4050, 52.5200, longitude, latitude) as dist
-FROM
-    listings
-WHERE 
-    minimum_nights  <= 6
-ORDER BY
-    dist 
+    cast(minimum_nights as integer)                             minimum_nights,
+    (toFloat64OrNull(replaceRegexpAll(price, '[$,]', '')))*7    week_price,
+    toFloat64OrNull(latitude)                                   latitude,
+    toFloat64OrNull(longitude)                                  longitude,
+    geoDistance(13.4050, 52.5200, longitude, latitude)          dist
+FROM listings
+WHERE minimum_nights  <= 6
+ORDER BY 8 
 LIMIT 10
--- Task_reviews.sql
+++ Task_reviews.sql
@@ -1,33 +1,32 @@
--- Task_distance
--- Цель: Найти доступные (available='t') объявления, у которых число отзывов от уникальных пользователей выше среднего.
--- Создать подзапрос
--- Объединить таблицы
--- Вывести топ 10 объявлений по заданному запросу и кол-во отзывов по каждому из них
-
+/*
+Task_distance
+Цель: Найти доступные (available='t') объявления, у которых число отзывов от уникальных пользователей выше среднего.
+Создать подзапрос
+Объединить таблицы
+Вывести топ 10 объявлений по заданному запросу и кол-во отзывов по каждому из них
+*/
 
 WITH
 (SELECT AVG(review_num) FROM (SELECT COUNT(DISTINCT(reviewer_id)) review_num, listing_id  FROM reviews GROUP BY listing_id)) AS avg_review
-
-
+--
 SELECT
-    listing_id,
-    review_num
+    listing_id, review_num
 FROM (
     SELECT
         COUNT(DISTINCT(reviewer_id)) review_num,
         listing_id
     FROM reviews
-    GROUP BY listing_id
+    GROUP BY 2
     ) AS r
 JOIN (
     SELECT
         listing_id
     FROM calendar_summary
     WHERE available='t' 
-    GROUP BY listing_id
+    GROUP BY 1
     ) AS c
 ON
 	c.listing_id = r.listing_id
 WHERE review_num > avg_review  
-ORDER BY listing_id
+ORDER BY 1
 LIMIT 10
